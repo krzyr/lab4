673cb1e899842fb607dbaf2f235f447c179c1e12
krzyr <avatimus@gmail.com>
2023-06-26 23:01:07 +0200

    Logi 30

--- main.sh
+++ main.sh
@@ -4,11 +4,20 @@ case $1 in
     shift
 	;;
 	--logs)
-		for ((i=1; i<=100; i++)); do
+	if [ $# -eq 2 ]
+	then
+        for ((i=1; i<=$2; i++)); do
             echo "Nazwa pliku: log$i.txt" > log$i.txt
-            echo "Skrypt: main.sh" >> log$i.txt
+            echo "Skrypt: main[[.sh" >> log$i.txt
             echo "Data: $(date)" >> log$i.txt
         done 
+    else
+        for ((i=1; i<=100; i++)); do
+            echo "Nazwa pliku: log$i.txt" > log$i.txt
+            echo "Skrypt: main[[.sh" >> log$i.txt
+            echo "Data: $(date)" >> log$i.txt
+        done 
+	fi
 	shift
 	;;
 	*)
@@ -16,3 +25,5 @@ case $1 in
     shift
 	;;
 esac
+
+
