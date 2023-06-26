6625e4862765973e6f882874bf5a55c6a6af72bd
krzyr <avatimus@gmail.com>
2023-06-26 20:03:28 +0200

    100 logow

--- main.sh
+++ main.sh
@@ -3,6 +3,14 @@ case $1 in
 		echo $(date)
     shift
 	;;
+	--logs)
+		for ((i=1; i<=100; i++)); do
+            echo "Nazwa pliku: log$i.txt" > log$i.txt
+            echo "Skrypt: main.sh" >> log$i.txt
+            echo "Data: $(date)" >> log$i.txt
+        done 
+	shift
+	;;
 	*)
         echo "Zly parametr"
     shift
